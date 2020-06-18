a7acf15b4ed258333be5155bd249af02b4dde59d
pratham choudhary <prathamchoudhary27037@gmail.com>
2020-06-18 22:50:11 +0530

    linked list

---
+++ c++program/linkedlist/inserting-a-node.cpp
@@ -0,0 +1,71 @@
+//inserting node using all 3 methods
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+
+//to enter the node at the begenning
+void push(Node** head_ref,int new_data){
+    Node* new_node = new Node();
+    new_node->data=new_data;
+    new_node->next=(*head_ref);
+    (*head_ref)=new_node;
+}
+
+//to add node after the given node
+void insertAfter(Node* prev_node,int new_data){
+    if(prev_node == NULL){
+        cout<<"previous node can not be null";
+        return;
+    }
+
+    Node* new_node = new Node();
+    new_node->data=new_data;
+    new_node->next=prev_node->next;
+    prev_node->next=new_node;
+}
+
+//to add at the end
+void append(Node** head_ref,int new_data){
+    Node* new_node = new Node();
+    Node* last = *head_ref;
+
+    new_node->data=new_data;
+    new_node->next=NULL;
+
+    if(*head_ref == NULL){
+        *head_ref=new_node;
+        return;
+    }
+
+    while(last->next != NULL)
+        last=last->next;
+    last->next = new_node;
+    return;
+}
+
+//to print the linked list
+void printlist(Node* node){
+    while(node != NULL){
+        cout<<" "<<node->data;
+        node = node->next;
+    }
+}
+
+// main function to call the upper functions
+int main(){
+    Node* head=NULL;
+    append(&head,6);
+    push(&head,7);
+    push(&head,1);
+    append(&head,4);
+    insertAfter(head->next,8);
+    cout<<"elements in Linked list";
+    printlist(head);
+    return 0;
+}
