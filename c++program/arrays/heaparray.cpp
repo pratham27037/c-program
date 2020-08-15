729491329466a904850a0c041a7ee9be2970dc73
pratham choudhary <prathamchoudhary27037@gmail.com>
2020-08-15 22:09:26 +0530

    heap array

---
+++ c++program/arrays/heaparray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int *p;
+    p = new int[5];
+    p[0]=1;
+    p[1]=2;
+    p[2]=3;
+    p[3]=4;
+    p[4]=5;
+
+    for(int i=0;i<5;i++){
+        cout<<p[i]<<" ";
+    }
+    return 0;
+}
